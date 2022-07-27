4360920de6a319d6a63b651d9f19d5a7012bf45f
timo-eberl <timo.eberl2000@gmail.com>
2022-07-27 22:41:24 +0200

    input setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Range(0f, 6f)] private float walkSpeed = 3f;
+    [SerializeField, Range(0f, 6f)] private float sprintSpeed = 6f;
+    [SerializeField, Min(0f)] private float acceleration = 8f;
+    [SerializeField, Min(0f)] private float deceleration = 16f;
+
+    private CharacterController characterController;
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction sprintAction;
+
+    private void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        if (playerInput == null)
+        {
+            playerInput = new PlayerInput();
+        }
+        playerInput.Player.Enable();
+        moveAction = playerInput.asset.FindAction("Player/Move");
+        sprintAction = playerInput.asset.FindAction("Player/Sprint");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        bool sprintInput = sprintAction.IsPressed();
+
+        Debug.Log("moveInput: " + moveInput);
     }
 }
