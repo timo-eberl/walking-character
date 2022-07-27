7953d6b0bd09434982a2fda1232961f662e31a82
timo-eberl <timo.eberl2000@gmail.com>
2022-07-27 23:05:48 +0200

    Player Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
     [SerializeField, Range(0f, 6f)] private float walkSpeed = 3f;
     [SerializeField, Range(0f, 6f)] private float sprintSpeed = 6f;
     [SerializeField, Min(0f)] private float acceleration = 8f;
     [SerializeField, Min(0f)] private float deceleration = 16f;
+    [SerializeField, Range(0f, 1f)] private float rotationSpeed = 0.8f;
 
     private CharacterController characterController;
     private PlayerInput playerInput;
     private InputAction moveAction;
     private InputAction sprintAction;
 
+    private float currentVelocity = 0f;
+    private float currentAngle = 0f;
+    private float currentAngleVelocity;
+
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        if (playerInput == null)
-        {
-            playerInput = new PlayerInput();
-        }
+
+        playerInput = new PlayerInput();
         playerInput.Player.Enable();
         moveAction = playerInput.asset.FindAction("Player/Move");
         sprintAction = playerInput.asset.FindAction("Player/Sprint");
@@ -33,9 +37,17 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        bool sprintInput = sprintAction.IsPressed();
+        Vector2 moveInputDirection = moveAction.ReadValue<Vector2>();
+        bool sprintInputPressed = sprintAction.IsPressed();
+        bool moveInputPressed = moveAction.IsPressed();
 
-        Debug.Log("moveInput: " + moveInput);
+        // rotation
+        float targetAngle = currentAngle;
+        if (moveInputPressed)
+        {
+            targetAngle = Mathf.Atan2(moveInputDirection.x, moveInputDirection.y) * Mathf.Rad2Deg;
+        }
+        currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentAngleVelocity, 1f - rotationSpeed) % 360f;
+        playerTransform.rotation = Quaternion.Euler(0, currentAngle, 0);
     }
 }
